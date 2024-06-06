eb2963f00a151834d822fa89ef90158f703341bc
120140002_AndikaRizkiRamadhan <95036143+andirizram@users.noreply.github.com>
2024-06-06 21:39:58 +0700

    Delete file original _resources_ directory

--- file original _resources_/js/auth.js
+++
@@ -1,7 +0,0 @@
-import jQuery from 'jquery';
-
-import vanillaSlideShow from '../assets/signin/vanilla/js/vanillaSlideshow.js'
-
-
-window.jQuery = window.$ = jQuery;
-window.vanillaSlideshow = vanillaSlideShow;
